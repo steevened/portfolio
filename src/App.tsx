16d2ca0f5b503f07ce35d29ede9eafadef039eb4
steven <alvaradosteven315@gmail.com>
2023-02-08 20:08:36 -0500

    move navbar into hero

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import Navbar from './components/Navbar';
 import tw from 'twin.macro';
 
 const styles = {
-  // Move long class sets out of jsx to keep it scannable
   container: [tw`min-h-screen`, tw`bg-primary`],
 };
 
 function App() {
   return (
     <div css={styles.container}>
-      <Navbar />
       <Hero />
     </div>
   );
