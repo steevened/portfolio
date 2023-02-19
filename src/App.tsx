556b87ef3bffabe65d3d83fbf1f00a6c3e0eb6d3
steven <alvaradosteven315@gmail.com>
2023-02-19 03:24:09 -0500

    working on dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,157 @@
 import './styles/App.css';
-import './styles/custom/backgrounds.css';
-import Hero from './components/Hero';
-import tw from 'twin.macro';
-import OverlayComponent from './components/Overlay';
-import { useModalStore } from './store/ModalStore';
-import { shallow } from 'zustand/shallow';
-import Services from './components/Services';
-import { useEffect } from 'react';
-import Navbar from './components/Navbar';
-import { Menu, Transition } from '@headlessui/react';
+import { useState, useEffect, Fragment, ReactNode } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
 
-const styles = {
-  container: [
-    tw`isolate bg-white  text-white`,
-    tw`dark:(bg-gray-999 text-gray-999)`,
-  ],
+const moonIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+      />
+    </svg>
+  );
 };
 
-function App() {
-  useEffect(() => {
+const sunIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+      />
+    </svg>
+  );
+};
+
+const systemIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"
+      />
+    </svg>
+  );
+};
+
+interface Options {
+  icon: () => JSX.Element;
+  text: string;
+}
+
+const options: Array<Options> = [
+  {
+    icon: systemIcon,
+    text: 'system',
+  },
+  {
+    icon: sunIcon,
+    text: 'dark',
+  },
+  {
+    icon: moonIcon,
+    text: 'light',
+  },
+];
+
+export default function App() {
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') ? localStorage.getItem('theme') : 'system'
+  );
+  const element = document.documentElement;
+  const darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+  const onWindowMatch = () => {
     if (
       localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
+      (!('theme' in localStorage) && darkQuery.matches)
     ) {
-      document.documentElement.classList.add('dark');
+      element.classList.add('dark');
     } else {
-      document.documentElement.classList.remove('dark');
+      element.classList.remove('dark');
     }
+  };
 
-    addEventListener('load', () =>
-      document.documentElement.classList.add('loaded')
-    );
-  }, []);
+  onWindowMatch();
 
-  const { isOpen } = useModalStore(
-    (state) => ({
-      isOpen: state.isOpen,
-    }),
-    shallow
-  );
+  useEffect(() => {
+    switch (theme) {
+      case 'dark':
+        element.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+        break;
+      case 'light':
+        element.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+        break;
+      default:
+        localStorage.removeItem('theme');
+        onWindowMatch();
+        break;
+    }
+  }, [theme]);
+
+  console.log(theme);
+
+  darkQuery.addEventListener('change', (e) => {
+    if (!('theme' in localStorage)) {
+      if (e.matches) {
+        element.classList.add('dark');
+      } else {
+        element.classList.remove('dark');
+      }
+    }
+  });
 
   return (
-    <div className="backgrounds" css={styles.container}>
-      <OverlayComponent isOpen={isOpen} />
-      <Navbar />
-      {/* <Hero /> */}
-      <Services />
+    <div className="dark:bg-slate-900 dark:text-gray-100 h-full min-h-screen duration-100">
+      <div className="fixed top-5 right-10 duration-100 dark:bg-slate-800 bg-gray-100 rounded flex">
+        <Listbox value={theme} onChange={setTheme}>
+          <Listbox.Button>{theme}</Listbox.Button>
+          <Listbox.Options>
+            {options.map((option) => (
+              <Listbox.Option value={option}>{option.icon()}</Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Listbox>
+      </div>
     </div>
   );
 }
 
-export default App;
+// {options.map((option) => (
+//   <button
+//     key={option.text}
+//     onClick={() => setTheme(option.text)}
+//     className={`w-8 h-8 leading-9 text-xl rounded-full m-1  flex items-center justify-center ${
+//       theme === option.text && 'text-sky-500'
+//     }`}
+//   >
+//     {option.icon()}
+//   </button>
+// ))}
