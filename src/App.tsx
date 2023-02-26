9e46a6c32709d9b43438c651377cd0cc3f89ef13
steven <alvaradosteven315@gmail.com>
2023-02-25 22:28:23 -0500

    build skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import './styles/App.css';
-import { useState, useEffect } from 'react';
-import ThemeToggle from './components/ThemeToggle';
 import Navbar from './components/Navbar';
 import Bg from './components/Bg';
 import Overlay from './components/overlay/Overlay';
-import Picture from './components/Picture';
-import NameCard from './components/InfoCards/NameCard';
-import Info from './components/Info';
-import Resume from './components/InfoCards/Resume';
-import LinkedinCard from './components/InfoCards/LinkedinCard';
-import GithubCard from './components/InfoCards/GithubCard';
 import Grid from './components/Grid';
 import Description from './components/Description';
 
