0c7f0d30139665466e45ea9cebdfcd4d46a27e8e
steven <alvaradosteven315@gmail.com>
2023-02-27 17:34:11 -0500

    set backgrounds works section

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Works from './components/works/Works';
 
 export default function App() {
   return (
-    <div className="relative h-full">
+    <div className="relative h-full overflow-x-hidden">
       <Bg />
       <div className="bg-slate-100 dark:bg-slate-900 dark:text-gray-100 h-full min-h-screen duration-100 px-5 pd:mx-8">
         <Navbar />
--- src/components/works/Works.tsx
+++ src/components/works/Works.tsx
@@ -2,26 +2,38 @@ import phoneBook from '../../assets/works/phonebook.png';
 import techcommerce from '../../assets/works/techcommerce.png';
 import githubfinder from '../../assets/works/githubfinder.png';
 import ViewContainer from '../ViewContainer';
+import bg from '../../assets/backgrounds/works.jpg';
+import bgDark from '../../assets/backgrounds/worksDark.jpg';
+
+const styles = {
+  backgroundImage: `url(${bg})`,
+  backgroundSize: '150rem',
+};
 
 export default function Works() {
   return (
-    <ViewContainer className="mt-12 p-10  ">
-      <h2 className="text-center py-10 text-2xl md:text-5xl font-bold">
-        Works
-      </h2>
-      <ViewContainer className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-5 p-10">
-        <div className="col-span-2">
-          <img src={githubfinder} alt="phonebook picture" />
-        </div>
-        <div className="grid gap-3">
-          <div>
-            <img src={phoneBook} alt="phonebook picture" />
+    <div className="relative">
+      <div
+        className={`absolute inset-y-0 -left-10 -right-10  bg-bottom bg-no-repeat bg-fixed bg-slate-50 dark:bg-[#0B1120] bg`}
+      >
+        <div className="absolute inset-0 bg-grid bg-fixed" />
+      </div>
+      <div className=" backdrop-blur-0 py-10">
+        <h2 className="text-slate-900 dark:text-white text-4xl font-extrabold pt-10 text-center">
+          Check out my recent works
+        </h2>
+        <div className="flex justify-center gap-4 mt-5">
+          <div className="w-64 ">
+            <img src={githubfinder} alt="githubfinder" />
           </div>
-          <div>
-            <img src={techcommerce} alt="phonebook picture" />
+          <div className="w-64 ">
+            <img src={techcommerce} alt="techcommerce" />
+          </div>
+          <div className="w-64 ">
+            <img src={phoneBook} alt="phone" />
           </div>
         </div>
-      </ViewContainer>
-    </ViewContainer>
+      </div>
+    </div>
   );
 }
